6868b02e39b94aba9af7d4323affe4debb5095f4
yimianyilian <yimianyilian@qq.com>
2020-10-26 23:53:44 +0800

    project  leyou   version 02

--- leyou-search/src/main/java/com/leyou/search/listener/GoodsListener.java
+++ leyou-search/src/main/java/com/leyou/search/listener/GoodsListener.java
@@ -1,4 +1,14 @@
 package com.leyou.search.listener;
 
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class GoodsListener {
+
+    @RabbitListener(bindings =)
+    public void save(Long id){
+
+    }
+
 }

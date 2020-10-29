fb295960d6064668efb0dccc85797964090709a5
yimianyilian <yimianyilian@qq.com>
2020-10-29 23:12:10 +0800

    project  leyou   version 03

--- leyou-sms/src/main/java/com/leyou/sms/sms/config/SmsProperties.java
+++
@@ -1,9 +0,0 @@
-package com.leyou.sms.sms.config;
-
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-
-@ConfigurationProperties(prefix = "leyou.sms")
-public class SmsProperties {
-}
